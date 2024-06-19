78baab90323708af8f67df831f9ad0228a482dcd
NazarHussain12 <nazarliaqat123@gmail.com>
2024-06-19 14:46:07 +0500

    updaed

--- dist/js/pages/dashboard.js
+++ dist/js/pages/dashboard.js
@@ -186,9 +186,9 @@ $(function () {
     resize: true,
     colors: ["#3c8dbc", "#f56954", "#00a65a"],
     data: [
-      {label: "Download Sales", value: 12},
-      {label: "In-Store Sales", value: 30},
-      {label: "Mail-Order Sales", value: 20}
+      {label: "Out Of Service", value: 12},
+      {label: "En Route", value: 30},
+      {label: "Available", value: 20}
     ],
     hideHover: 'auto'
   });
